650844372ca33076c8c23fec1ea5b4c9db2a76c3
khoirulanamirfanudin <choianam75@gmail.com>
2022-10-27 14:38:35 +0700

    menambahkan dan memodif file day-11

--- main.go
+++ main.go
@@ -2,28 +2,46 @@ package main
 
 import (
 	"context"
-	"day-9/connection"
+	"day-11/connection"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"strconv"
-	"time"
+	"strings"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"golang.org/x/crypto/bcrypt"
 )
 
-var Data = map[string]interface{}{
-	"Title": "Personal Web",
+type MetaData struct {
+	Title     string
+	IsLogin   bool
+	UserName  string
+	FlashData string
+}
+
+var Data = MetaData{
+	Title: "Personal Web",
 }
 
 type Card struct {
-	Id          int
-	Title       string
-	Post_date   time.Time
-	Format_date string
+	Id         int
+	Name       string
+	Start_date string
+	End_date   string
+	// Technologies string
 	Author      string
-	Content     string
+	Description string
+	IsLogin     bool
+}
+
+type User struct {
+	Id       int
+	Name     string
+	Email    string
+	Password string
 }
 
 var Cards = []Card{
@@ -48,10 +66,19 @@ func main() {
 	route.HandleFunc("/", home).Methods("GET")
 	route.HandleFunc("/contact", contact).Methods("GET")
 	route.HandleFunc("/addProject", addProject).Methods("GET")
-	route.HandleFunc("/card-detail/{index}", cardDetail).Methods("GET")
+	route.HandleFunc("/card-detail/{id}", cardDetail).Methods("GET")
 	route.HandleFunc("/form-card", formAddCard).Methods("GET")
 	route.HandleFunc("/add-card", addCard).Methods("POST")
-	route.HandleFunc("/delete/{index}", delete).Methods("GET")
+	route.HandleFunc("/edit/{id}", edit).Methods("GET")
+	route.HandleFunc("/delete/{id}", delete).Methods("GET")
+
+	route.HandleFunc("/form-register", formRegister).Methods("GET")
+	route.HandleFunc("/register", register).Methods("POST")
+
+	route.HandleFunc("/form-login", formLogin).Methods("GET")
+	route.HandleFunc("/login", login).Methods("POST")
+
+	route.HandleFunc("/logout", logout).Methods("GET")
 
 	fmt.Println("Server running on port 5000")
 	http.ListenAndServe("localhost:5000", route)
@@ -71,8 +98,30 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var store = sessions.NewCookieStore([]byte("SESSION_KEY"))
+	session, _ := store.Get(r, "SESSION_KEY")
+
+	if session.Values["IsLogin"] != true {
+		Data.IsLogin = false
+	} else {
+		Data.IsLogin = session.Values["IsLogin"].(bool)
+		Data.UserName = session.Values["Name"].(string)
+	}
+
+	fm := session.Flashes("message")
+
+	var flashes []string
+	if len(fm) > 0 {
+		session.Save(r, w)
+		for _, fl := range fm {
+			flashes = append(flashes, fl.(string))
+		}
+	}
+
+	Data.FlashData = strings.Join(flashes, "")
+
 	w.WriteHeader(http.StatusOK)
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, Data)
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
@@ -85,8 +134,36 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var Data = MetaData{}
+
+	var store = sessions.NewCookieStore([]byte("SESSION_KEY"))
+	session, _ := store.Get(r, "SESSION_KEY")
+
+	if session.Values["IsLogin"] != true {
+		Data.IsLogin = false
+	} else {
+		Data.IsLogin = session.Values["IsLogin"].(bool)
+		Data.UserName = session.Values["Name"].(string)
+	}
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	if session.Values["IsLogin"] != true {
+		Data.IsLogin = false
+	} else {
+		Data.IsLogin = session.Values["IsLogin"].(bool)
+	}
+
+	data := map[string]interface{}{
+		"Data": Data,
+	}
+
 	w.WriteHeader(http.StatusOK)
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, data)
 }
 
 func addProject(w http.ResponseWriter, r *http.Request) {
@@ -101,14 +178,24 @@ func addProject(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	rows, _ := connection.Conn.Query(context.Background(), "SELECT id, title, content FROM tb_blog")
+	var store = sessions.NewCookieStore([]byte("SESSION_KEY"))
+	session, _ := store.Get(r, "SESSION_KEY")
+
+	if session.Values["IsLogin"] != true {
+		Data.IsLogin = false
+	} else {
+		Data.IsLogin = session.Values["IsLogin"].(bool)
+		Data.UserName = session.Values["Name"].(string)
+	}
+
+	rows, _ := connection.Conn.Query(context.Background(), "SELECT id, name, start_date, end_date, description FROM tb_projects")
 
 	var result []Card
 
 	for rows.Next() {
 		var each = Card{}
 
-		var err = rows.Scan(&each.Id, &each.Title, &each.Content)
+		var err = rows.Scan(&each.Id, &each.Name, &each.Start_date, &each.End_date, &each.Description)
 
 		if err != nil {
 			fmt.Println(err.Error())
@@ -118,12 +205,19 @@ func addProject(w http.ResponseWriter, r *http.Request) {
 		each.Author = "Khoirul Anam Irfanudin"
 		// each.Format_date = each.Post_date.Format("3 Maret 2008")
 
+		if session.Values["IsLogin"] != true {
+			each.IsLogin = false
+		} else {
+			each.IsLogin = session.Values["IsLogin"].(bool)
+		}
+
 		result = append(result, each)
 	}
 
 	fmt.Println(result)
 
 	respData := map[string]interface{}{
+		"Data":  Data,
 		"Cards": result,
 	}
 
@@ -134,6 +228,8 @@ func addProject(w http.ResponseWriter, r *http.Request) {
 func cardDetail(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
 	var tmpl, err = template.ParseFiles("views/cardDetail.html")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -141,22 +237,49 @@ func cardDetail(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var CardDetail = Card{}
+	var Data = MetaData{}
 
-	index, _ := strconv.Atoi(mux.Vars(r)["index"])
+	var store = sessions.NewCookieStore([]byte("SESSION_KEY"))
+	session, _ := store.Get(r, "SESSION_KEY")
 
-	for i, data := range Cards {
-		if index == i {
-			CardDetail = Card{
-				Title:     data.Title,
-				Content:   data.Content,
-				Post_date: data.Post_date,
-				Author:    data.Author,
-			}
-		}
+	if session.Values["IsLogin"] != true {
+		Data.IsLogin = false
+	} else {
+		Data.IsLogin = session.Values["IsLogin"].(bool)
+		Data.UserName = session.Values["Name"].(string)
 	}
 
+	var CardDetail = Card{}
+
+	err = connection.Conn.QueryRow(context.Background(), "SELECT id, name, start_date, end_date, description FROM tb_projects WHERE id=$1", id).Scan(
+		&CardDetail.Id, &CardDetail.Name, &CardDetail.Start_date, &CardDetail.End_date, &CardDetail.Description,
+	)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	if session.Values["IsLogin"] != true {
+		Data.IsLogin = false
+	} else {
+		Data.IsLogin = session.Values["IsLogin"].(bool)
+	}
+
+	// for i, data := range Cards {
+	// 	if index == i {
+	// 		CardDetail = Card{
+	// 			Name:        data.Name,
+	// 			Description: data.Description,
+	// 			Start_date:  data.Start_date,
+	// 			End_date:    data.End_date,
+	// 			Author:      data.Author,
+	// 		}
+	// 	}
+	// }
+
 	data := map[string]interface{}{
+		"Data": Data,
 		"Card": CardDetail,
 	}
 
@@ -176,8 +299,36 @@ func formAddCard(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var Data = MetaData{}
+
+	var store = sessions.NewCookieStore([]byte("SESSION_KEY"))
+	session, _ := store.Get(r, "SESSION_KEY")
+
+	if session.Values["IsLogin"] != true {
+		Data.IsLogin = false
+	} else {
+		Data.IsLogin = session.Values["IsLogin"].(bool)
+		Data.UserName = session.Values["Name"].(string)
+	}
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	if session.Values["IsLogin"] != true {
+		Data.IsLogin = false
+	} else {
+		Data.IsLogin = session.Values["IsLogin"].(bool)
+	}
+
+	data := map[string]interface{}{
+		"Data": Data,
+	}
+
 	w.WriteHeader(http.StatusOK)
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, data)
 }
 
 func addCard(w http.ResponseWriter, r *http.Request) {
@@ -186,30 +337,179 @@ func addCard(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Title : " + r.PostForm.Get("inputTitle")) // value berdasarkan dari tag input name
-	fmt.Println("Content : " + r.PostForm.Get("inputContent"))
+	fmt.Println("Name : " + r.PostForm.Get("inputName")) // value berdasarkan dari tag input name
+	fmt.Println("Description : " + r.PostForm.Get("inputDescriptiom"))
+	fmt.Println("Start_date : " + r.PostForm.Get("inputStartDate"))
+	fmt.Println("End_date : " + r.PostForm.Get("inputEndDate"))
+	// fmt.Println("Technologies : " + r.PostForm.Get("inputTechnologies"))
 
-	var title = r.PostForm.Get("inputTitle")
-	var content = r.PostForm.Get("inputContent")
+	var name = r.PostForm.Get("inputName")
+	var description = r.PostForm.Get("inputDescription")
+	var start_date = r.PostForm.Get("inputStartDate")
+	var end_date = r.PostForm.Get("inputEndDate")
+	// var technologies = r.PostForm.Get("inputTechnologies")
 
-	var newCard = Card{
-		Title:   title,
-		Content: content,
-		Author:  "Khoirul Anam Irfanudin",
+	// var newCard = Card{
+	// 	Name:        name,
+	// 	Description: description,
+	// 	Start_date:  start_date,
+	// 	End_date:    end_date,
+	// 	Author:      "Khoirul Anam Irfanudin",
+	// }
+
+	// Cards = append(Cards, newCard)
+	// // fmt.Println(Cards)
+
+	_, err = connection.Conn.Exec(context.Background(), "INSERT INTO tb_projects(name, start_date, end_date, description) VALUES ($1, $2, $3, $4) ", name, start_date, end_date, description)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
 	}
-
-	Cards = append(Cards, newCard)
-	// fmt.Println(Cards)
 
 	http.Redirect(w, r, "/addProject", http.StatusMovedPermanently)
 }
 
+func edit(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	fmt.Println(id)
+
+	_, err := connection.Conn.Exec(context.Background(), "DELETE FROM tb_projects WHERE id=$1", id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+	}
+
+	http.Redirect(w, r, "/form-card", http.StatusFound)
+
+}
+
 func delete(w http.ResponseWriter, r *http.Request) {
-	index, _ := strconv.Atoi(mux.Vars(r)["index"])
-	fmt.Println(index)
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	fmt.Println(id)
 
 	// Cards = append(Cards[:index], Cards[index+1:]...)
 	// fmt.Println(Cards)
 
+	_, err := connection.Conn.Exec(context.Background(), "DELETE FROM tb_projects WHERE id=$1", id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+	}
+
 	http.Redirect(w, r, "/addProject", http.StatusFound)
+}
+
+func formRegister(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	var tmpl, err = template.ParseFiles("views/formRegister.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+	}
+
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, nil)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var name = r.PostForm.Get("inputUserName")
+	var email = r.PostForm.Get("inputEmail")
+	var password = r.PostForm.Get("inputPassword")
+
+	passwordHash, _ := bcrypt.GenerateFromPassword([]byte(password), 10)
+
+	fmt.Println(passwordHash)
+
+	_, err = connection.Conn.Exec(context.Background(), "INSERT INTO tb_user(name, email, password) VALUES($1, $2, $3)", name, email, passwordHash)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	http.Redirect(w, r, "/form-login", http.StatusMovedPermanently)
+}
+
+func formLogin(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	var tmpl, err = template.ParseFiles("views/formLogin.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	var store = sessions.NewCookieStore([]byte("SESSION_KEY"))
+	session, _ := store.Get(r, "SESSION_KEY")
+
+	fm := session.Flashes("message")
+
+	var flashes []string
+	if len(fm) > 0 {
+		session.Save(r, w)
+		for _, fl := range fm {
+			flashes = append(flashes, fl.(string))
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, Data)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	var store = sessions.NewCookieStore([]byte("SESSION_KEY"))
+	session, _ := store.Get(r, "SESSION_KEY")
+
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	email := r.PostForm.Get("inputEmail")
+	password := r.PostForm.Get("inputPassword")
+
+	user := User{}
+
+	err = connection.Conn.QueryRow(context.Background(), "SELECT * FROM tb_user WHERE email=$1", email).Scan(
+		&user.Id, &user.Name, &user.Email, &user.Password,
+	)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	session.Values["IsLogin"] = true
+	session.Values["Name"] = user.Name
+	session.Options.MaxAge = 10800 // 3 hours
+
+	session.AddFlash("Successfully Login!", "message")
+	session.Save(r, w)
+
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Logout!")
+	var store = sessions.NewCookieStore([]byte("SESSION_KEY"))
+	session, _ := store.Get(r, "SESSION_KEY")
+	session.Options.MaxAge = -1
+	session.Save(r, w)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
